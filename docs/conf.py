b2773fc28f6e758d65f1f7d56e3b3531dcb07311
Petr Papoušek <ppapousek@gmail.com>
2013-05-16 18:37:58 +0200

    docs conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -11,16 +11,24 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import sys
+import os
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "mezzanine.project_template.settings"
+# os.environ["DJANGO_SETTINGS_MODULE"] = "connect_four.local_settings"
+docs_path = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.join(docs_path, ".."))
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('..'))
 # sys.path.insert(0, os.path.abspath('../..'))
 
 # Set up the Django settings/environment
 # from django.core.management import setup_environ
+# from django.conf import settings
 # import settings
 # setup_environ(settings)
 
