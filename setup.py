b91a7be0aefcc4642c400db2638651663b3dc9c6
Petr Papoušek <ppapousek@gmail.com>
2013-05-16 17:05:26 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#       -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='connect_four',
+    version='1.0',
+    description="Connect four app for Django",
+    long_description="",
+    author='Petr Papousek',
+    author_email='ppapousek@gmail.org',
+    license='BSD',
+    packages=['connect_four'],
+    zip_safe=False,
+    # install_requires=[
+    #     'Django',
+    # ],
+)
