d7a0edfa871d56694a623423983e41ef9746ad03
Petr Papoušek <ppapousek@gmail.com>
2013-05-16 18:37:37 +0200

    make project load db independent

--- connect_four/forms.py
+++ connect_four/forms.py
@@ -5,7 +5,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
 from connect_four.models import Game
-from connect_four.opponents import computer_opponent_easy, opponents
+from connect_four.opponents import opponents
 
 
 class NewGameForm(forms.ModelForm):
@@ -27,7 +27,8 @@ class NewGameForm(forms.ModelForm):
     def save(self, commit=True):
         instance = super(NewGameForm, self).save(commit=False)
         if int(self.cleaned_data['opponent']) == opponents.computer_easy:
-            self.instance.player2 = computer_opponent_easy
+            from connect_four.opponents import get_computer_opponent
+            self.instance.player2 = get_computer_opponent()
         self.instance.init_state()
         self.instance.user_create = self.request.user
         self.instance.player1 = self.request.user
--- connect_four/opponents.py
+++ connect_four/opponents.py
@@ -1,8 +1,8 @@
 #       -*- coding: utf-8 -*-
-from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
 from model_utils import Choices
+
 from connect_four.models import ComputerOpponentEasy
 
 __author__ = 'papousek'
@@ -12,8 +12,10 @@ opponents = Choices(
     (2, 'computer_hard', _('computer (hard)')),
 )
 
-computer_opponent_easy = ComputerOpponentEasy.objects.get_or_create(
-    first_name="Easy",
-    last_name="Computer",
-)[0]
+
+def get_computer_opponent(difficulty):
+    return ComputerOpponentEasy.objects.get_or_create(
+        first_name="Easy",
+        last_name="Computer",
+    )[0]
 
